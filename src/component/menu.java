f20d40f2c89f6836c91097f558ddc09f64f8b0e9
Parikshit <parikshit9112gmail.com>
2023-07-15 01:58:56 +0530

    menu class updated

--- src/component/menu.java
+++ src/component/menu.java
@@ -20,7 +20,7 @@ public class menu extends javax.swing.JPanel {
         Graphics2D g2=(Graphics2D) grph;
         g2.setColor(getBackground());
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        GradientPaint gp=new GradientPaint(0,0,Color.decode("#4568dc"),0,getHeight(),Color.decode("#b06ab3"));
+        GradientPaint gp=new GradientPaint(0,0,Color.decode("#f6a09a"),0,getHeight(),Color.decode("#8a1f1d"));
         g2.setPaint(gp);
         g2.fillRoundRect(0, 0,getWidth(),getHeight(),20, 20);
         g2.fillRect(getWidth() -25,0,getWidth(),getHeight());
